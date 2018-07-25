097ce36552d7080f33d8f7e30d2c573e80f25544
Dennis Tomas <d.tomas@nachtblau.tv>
2018-07-25 17:48:40 +0200

    Group by fixture definition location.

--- pytest_fixture_tools/plugin.py
+++ pytest_fixture_tools/plugin.py
@@ -3,6 +3,10 @@
 import py
 import os
 import errno
+import inspect
+import sys
+#import pprint
+import functools
 
 from _pytest.python import getlocation
 from collections import defaultdict
@@ -144,27 +148,116 @@ def pytest_runtest_setup(item):
         )
 
 
+def _get_fixture_search_order(func_path):
+    func_dir = os.path.dirname(func_path)
+    if func_dir == func_path:
+        return []
+    conftest_path = os.path.join(func_dir, 'conftest.py')
+    return [conftest_path] + _get_fixture_search_order(func_dir)
+
+
+def _get_func_path(func, path_cache):
+    try:
+        return path_cache[func]
+    except KeyError:
+        path_cache[func] = func_path = inspect.getfile(func)
+        return func_path
+
+
+def _find_fixture_def(source_fixture_name, func_path, fixture_name, name2fixturedefs, get_func_path):
+    search_order = _get_fixture_search_order(os.path.dirname(func_path))
+
+    if source_fixture_name != fixture_name:
+        # Do not include same file in search path when overriding a fixture.
+        search_order.insert(0, func_path)
+
+    def sort_key(fixture_def):
+        try:
+            return search_order.index(get_func_path(fixture_def.func))
+        except ValueError:
+            return sys.maxsize
+    try:
+        target_fixture_defs = name2fixturedefs[fixture_name]
+    except KeyError:
+        return None
+    try:
+        fixture_def = sorted(
+            target_fixture_defs,
+            key=sort_key,
+        )[0]
+    except IndexError:
+        return None
+    return fixture_def
+
+
+def _get_cluster_name(func_path, cwd):
+    return os.path.relpath(func_path, cwd) if func_path.startswith(cwd) else func_path
+
+
+def _get_fixture_node_name(fixture_name, fixture_def, cwd, get_func_path):
+    if fixture_def is None:
+        return '', ''
+    func_path = get_func_path(fixture_def.func)
+    return _get_cluster_name(func_path, cwd), fixture_name
+
+
 def save_fixture_graph(config, name2fixturedefs, filename, func_args=None):
-    data = dict()
+    data = defaultdict(dict)
     if func_args:
-        data['func_args'] = func_args, 'red'
+        data['']['func_args'] = func_args, 'red'
+    cwd = os.getcwd() + os.sep
+
+    get_func_path = functools.partial(_get_func_path, path_cache={})
+
     for fixture_name, fixture_defs in list(name2fixturedefs.items()):
+        if fixture_name == 'request':
+            continue
 
         for fixture_def in fixture_defs:
+            func_path = get_func_path(fixture_def.func)
+            cluster_name = _get_cluster_name(func_path, cwd)
             color = 'green'
-            data[fixture_name] = fixture_def.argnames, color
-            break
+            data[cluster_name][fixture_name] = [
+                _get_fixture_node_name(
+                    argname, _find_fixture_def(
+                        fixture_name, func_path, argname, name2fixturedefs, get_func_path,
+                    ), cwd, get_func_path,
+                )
+                for argname in fixture_def.argnames
+            ], color
+
+    #print(pprint.pformat(dict(data)))
 
     graph = pydot.Dot(graph_type='digraph')
+    #graph.set_splines('true')
+    graph.set_concentrate('true')
+    graph.set_rankdir('LR')
+    #graph.set_overlap('compress')
+    #graph.set_ratio('compress')
 
-    for name, depended_list in list(data.items()):
-        depended_list, color = depended_list
+    for func_path, subgraph_data in data.items():
 
-        node = pydot.Node(name, style="filled", fillcolor=color)
-        graph.add_node(node)
-        for i in depended_list:
-            edge = pydot.Edge(node, i)
-            graph.add_edge(edge)
+        subgraph = pydot.Cluster(graph_name=func_path)
+        #subgraph.set_splines('true')
+        subgraph.set_label(func_path)
+        subgraph.set_concentrate('true')
+        #subgraph.set_overlap('compress')
+        #subgraph.set_ratio('compress')
+        #subgraph.set_size(1)
+        graph.add_subgraph(subgraph)
+
+        for name, depended_list in list(subgraph_data.items()):
+            depended_list, color = depended_list
+
+            node = pydot.Node(func_path + "/" + name, style="filled", fillcolor=color)
+            node.set_label(name)
+            subgraph.add_node(node)
+            for dest_cluster, dest_name in depended_list:
+                if not dest_name:
+                    continue
+                edge = pydot.Edge(node, dest_cluster + '/' + dest_name)
+                graph.add_edge(edge)
+                #subgraph.set_ltail(dest_cluster)
 
     log_dir = config.option.fixture_graph_output_dir
     output_type = config.option.fixture_graph_output_type
@@ -172,11 +265,11 @@ def save_fixture_graph(config, name2fixturedefs, filename, func_args=None):
     filename = os.path.join(log_dir, filename)
     tw.line()
     tw.sep("-", "fixture-graph")
+    tw.line("created {}.dot.".format(filename))
+    graph.write(filename + ".dot")
     try:
         graph.write("{}.{}".format(filename, output_type), format=output_type)
         tw.line("created {}.{}.".format(filename, output_type))
     except Exception:
         tw.line("graphvis wasn't found in PATH")
-        graph.write(filename + ".dot")
-        tw.line("created {}.dot.".format(filename))
         tw.line("You can convert it to a PNG using:\n\t'dot -Tpng {0}.dot -o {0}.png'".format(filename))
